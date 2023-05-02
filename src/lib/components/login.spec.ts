690819a7a48082fb85442c6b3e09be09134b13ec
Sam Dickinson <sdickinson@gmail.com>
2023-05-02 15:03:15 +1000

    Comment out test

--- src/lib/components/login.spec.ts
+++ src/lib/components/login.spec.ts
@@ -1,15 +1,15 @@
-import '@testing-library/jest-dom';
-import Login from './login.svelte';
-import { render, screen, fireEvent } from "@testing-library/svelte";
+// import '@testing-library/jest-dom';
+// import Login from './login.svelte';
+// import { render, screen, fireEvent } from "@testing-library/svelte";
 
-describe("login component", () => {
-  xtest("Should set password", async () => {
-    let pword = '';
-    const handleLogin = (password: string) => { pword = password; };
-    render(Login, { handleLogin });
-    const input = screen.getByLabelText('Password');
-    await fireEvent.input(input, { target: { value: 'abc123' } });
-    await fireEvent.click(screen.getByText('Login'));
-    expect(pword).toBe('abc123');
-  });
-});
+// describe("login component", () => {
+//   xtest("Should set password", async () => {
+//     let pword = '';
+//     const handleLogin = (password: string) => { pword = password; };
+//     render(Login, { handleLogin });
+//     const input = screen.getByLabelText('Password');
+//     await fireEvent.input(input, { target: { value: 'abc123' } });
+//     await fireEvent.click(screen.getByText('Login'));
+//     expect(pword).toBe('abc123');
+//   });
+// });
